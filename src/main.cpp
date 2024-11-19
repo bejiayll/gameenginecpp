bf5035fbd4f097cd74ba5e2df38b180cee7e4ddd
BeJIayLLI <79015676+bejiayll@users.noreply.github.com>
2024-11-19 08:28:22 +0400

    Added and initiaized GLAD

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,6 @@
+#include <glad/gl.h>
 #include <GLFW/glfw3.h>
-
+#include <iostream>
 int main(void)
 {
     GLFWwindow* window;
@@ -16,18 +17,24 @@ int main(void)
         return -1;
     }
 
-    /* Make the window's context current */
     glfwMakeContextCurrent(window);
 
+    int version = gladLoadGL(glfwGetProcAddress);
+    if (!version){
+        std::cout << "GLAD not init.\n";
+        return -1;
+    }
+
+    std::cout << "Init info: \nOpenGl\t" << GLAD_VERSION_MAJOR(version) << "." << GLAD_VERSION_MINOR(version) << std::endl;
+
+    /* Make the window's context current */
+    glClearColor(0.83, 1, 0, 1);
     /* Loop until the user closes the window */
     while (!glfwWindowShouldClose(window))
     {
-        /* Render here */
-        //glClear(GL_COLOR_BUFFER_BIT);
-
+        glClear(GL_COLOR_BUFFER_BIT);
         /* Swap front and back buffers */
         glfwSwapBuffers(window);
-
         /* Poll for and process events */
         glfwPollEvents();
     }
